0e61037684a0d43078cf0fabceac93203b0ec8d3
nemoto <samuko.atuatu@gmail.com>
2020-04-18 10:17:09 +0000

    20.4.18-1 1st commit

---
+++ resources/views/admin/amazake/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>甘酒情報登録</title>
+    </head>
+    <body>
+        <h1>甘酒情報登録画面</h1>
+    </body>
+</html>
